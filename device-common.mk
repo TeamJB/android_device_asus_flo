fcbf7bdae76140626f625b832a29eb8ff3ec54af
Dorian Snyder <dastin1015@gmail.com>
2013-10-12 00:09:36 -0700

    flo: inherit linaro toolchain config to use Linaro GCC 4.8

--- device-common.mk
+++ device-common.mk
@@ -285,3 +285,4 @@ PRODUCT_LOCALES := en_US es_US de_DE zh_CN
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+$(call inherit-product, device/linaro/common/linaro_configs.mk)
