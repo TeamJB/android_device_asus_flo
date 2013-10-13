ce0e96a6c7351d4924a2c6e93a36a652b46a07fe
Dorian Snyder <dastin1015@gmail.com>
2013-10-12 20:35:59 -0700

    Revert "flo: inherit linaro toolchain config to use Linaro GCC 4.8"
    This reverts commit fcbf7bdae76140626f625b832a29eb8ff3ec54af.

--- device-common.mk
+++ device-common.mk
@@ -285,4 +285,3 @@ PRODUCT_LOCALES := en_US es_US de_DE zh_CN
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
-$(call inherit-product, device/linaro/common/linaro_configs.mk)
