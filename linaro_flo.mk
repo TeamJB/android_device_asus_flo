5469734dca4a2bd9948c8da5cc86f24f82915f39
Dorian Snyder <dastin1015@gmail.com>
2013-10-26 03:13:02 -0700

    flo: compile kernel with Linaro gcc 4.7 toolchain

--- linaro_flo.mk
+++ linaro_flo.mk
@@ -10,3 +10,5 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.3/JSS15R/804956:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.3 JSS15R 804956 release-keys"
 
+TARGET_TOOLS_PREFIX ?= prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7-linaro/bin/arm-linux-androideabi-
+
