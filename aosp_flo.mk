207ea4f4b8ee6321cce9679ad961d708292e1d27
Dorian Snyder <dastin1015@gmail.com>
2014-06-22 01:57:11 -0700

    flo: update fingerprint to latest OTA

--- aosp_flo.mk
+++ aosp_flo.mk
@@ -28,7 +28,7 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.3/KTU84L/1148727:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.3 KTU84L 1148727 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT="google/razor/flo:4.4.4/KTU84P/1227136:user/release-keys" PRIVATE_BUILD_DESC="razor-user 4.4.4 KTU84P 1227136 release-keys"
 
 KERNEL_TOOLCHAIN_PREFIX ?= $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7-linaro/bin/arm-linux-androideabi-
 
